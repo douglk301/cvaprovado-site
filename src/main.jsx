9adea5c5b6426074d19bd0c8d9f4bc5c8d3ae05e
douglk301 <douglk3@gmail.com>
2026-05-26 23:30:35 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+    ],
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
